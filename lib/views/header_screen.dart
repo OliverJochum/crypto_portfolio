7e4701797479b5ea602be462a0bcf65b0884d3f8
Oliver Jochum <oliver.jochum@yahoo.com>
2023-05-28 21:19:10 +0200

    Added initial components

--- lib/views/header_screen.dart
+++ lib/views/header_screen.dart
@@ -3,16 +3,26 @@ import 'package:flutter/material.dart';
 class HeaderScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var currency = 'f';
     // TODO: implement build
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         IconButton(onPressed: onPressed, icon: Image.asset('app_logo.png')),
-        Row(children: [
-          Icon(icon),
-          Text(data),
-          ElevatedButton(onPressed: onPressed, child: child)
-        ],)
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0
+            ),
+            borderRadius: BorderRadius.circular(8.0)
+          ),
+          child: Row(children: [
+            Icon(icon),
+            Text(currency),
+            ElevatedButton(onPressed: onPressed, child: child)
+          ],),
+        )
       ],
     );
   }
