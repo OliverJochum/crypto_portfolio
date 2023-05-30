2a511b8c12ec5a181c59b73f8940c380f5c02777
Oliver Jochum <oliver.jochum@yahoo.com>
2023-05-30 16:40:40 +0200

    created

---
+++ lib/views/holdings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HoldingsScreen extends StatefulWidget{
+  const HoldingsScreen({Key? key}) : super(key:key);
+  @override
+  
+  // ignore: library_private_types_in_public_api
+  _HoldingScreenState createState() => _HoldingScreenState();
+}
+
+class _HoldingScreenState extends State<HoldingsScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
