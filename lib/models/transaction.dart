e785b89f4d9a2632783eaf84e56453fe8fe855b8
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-15 15:16:35 +0200

    added details getter

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -4,6 +4,15 @@ class Transaction{
   double amount;
   DateTime timestamp;
 
+  Map <String, dynamic> get details {
+    return {
+      "sender":sender,
+      "receiver":receiver,
+      "amount":amount,
+      "timestamp":timestamp
+    };
+  }
+
   Transaction(this.sender, this.receiver, this.amount, this.timestamp);
 }
 
