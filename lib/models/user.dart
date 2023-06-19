1946ff3be96ca8e4dad9cd1fbfa30b93bb490c1a
Oliver Jochum <oliver.jochum@yahoo.com>
2023-06-19 23:31:42 +0200

    Added user#newBTCWallet

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,21 @@
+import 'package:crypto_portfolio/models/btcWallet.dart';
 import 'package:crypto_portfolio/models/wallet.dart';
 
 class User{
   double value;
 
-  List<Wallet> wallets;
+  List<Wallet> wallets = List<Wallet>.empty(growable: true);
 
   User(this.value, this.wallets);
+
+  Wallet newBTCWallet(String address, String network){
+
+    if (network != "BTC") throw ("Network is incorrect. Expected: 'BTC' ");
+
+    Wallet wallet = BtcWallet(address);
+    wallets.add(wallet);
+
+    return wallet;
+  }
 }
+
