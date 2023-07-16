9f794cdef74b95e12bf01139fcae8b24d946e625
Oliver Jochum <oliver.jochum@yahoo.com>
2023-07-17 00:40:35 +0200

    Example user for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 
+import 'package:crypto_portfolio/viewmodels/header_viewmodel.dart';
+import 'package:crypto_portfolio/viewmodels/holdings_viewmodel.dart';
+import 'package:crypto_portfolio/viewmodels/wallets_viewmodel.dart';
 import 'package:crypto_portfolio/views/holdings_screen.dart';
+import 'package:crypto_portfolio/views/wallets_screen.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'models/user.dart';
 import 'views/holdings_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -31,16 +36,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+  
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    User user = User.empty();
+    user.newBTCWallet("18cBEMRxXHqzWWCxZNtU91F5sbUNKhL5PX", "BTC");
+    user.newBTCWallet("3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r", "BTC");
+    WalletsViewModel walletsViewModel = WalletsViewModel(user: user);
+    HoldingsViewModel holdingsViewModel = HoldingsViewModel(user: user);
+    HeaderViewModel headerViewModel = HeaderViewModel(user: user);
     return Scaffold(
-      body: HoldingsScreen()
+      body: HoldingsScreen(holdingsViewModel: holdingsViewModel,headerViewModel: headerViewModel,)
       );
     
   }
