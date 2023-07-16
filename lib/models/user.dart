22c92d9aa55c466d3bfdad4181dc20336a3c41bf
Oliver Jochum <oliver.jochum@yahoo.com>
2023-07-17 00:40:09 +0200

    Added calculateValue and .empty()

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,10 +2,14 @@ import 'package:crypto_portfolio/models/btcWallet.dart';
 import 'package:crypto_portfolio/models/wallet.dart';
 
 class User{
-  double value;
+  double value = 0.0;
 
   List<Wallet> wallets = List<Wallet>.empty(growable: true);
 
+  User.empty(){
+    value = 0.0;
+    wallets = List<Wallet>.empty(growable: true);
+  }
   User(this.value, this.wallets);
 
   Wallet newBTCWallet(String address, String network){
@@ -16,6 +20,13 @@ class User{
     wallets.add(wallet);
 
     return wallet;
+  }
+
+  Future<double> calculateValue(String currency) async{
+    for(Wallet wallet in wallets){
+      value += await wallet.getValue(currency);
+    }
+    return value;
   }
 }
 
