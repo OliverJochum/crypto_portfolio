a83641a1e780566d5a58a140f1fd43be95a502de
Oliver Jochum <oliver.jochum@yahoo.com>
2023-07-17 00:38:52 +0200

    Implemented Futurebuilder & corrected data display

--- lib/views/holdings_screen.dart
+++ lib/views/holdings_screen.dart
@@ -1,8 +1,14 @@
+import 'package:crypto_portfolio/viewmodels/holdings_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:crypto_portfolio/views/header_screen.dart';
 
+import '../models/wallet.dart';
+import '../viewmodels/header_viewmodel.dart';
+
 class HoldingsScreen extends StatefulWidget {
-  const HoldingsScreen({Key? key}) : super(key: key);
+  final HoldingsViewModel holdingsViewModel;
+  final HeaderViewModel headerViewModel;
+  const HoldingsScreen({super.key, required this.holdingsViewModel, required this.headerViewModel});
   @override
 
   // ignore: library_private_types_in_public_api
@@ -10,18 +16,59 @@ class HoldingsScreen extends StatefulWidget {
 }
 
 class _HoldingScreenState extends State<HoldingsScreen> {
+  final String _currency = "USD";
+
+  
+  late List<Future<String>> futureAmounts;
+  late List<Future<String>> futureValues;
+
+  @override
+  void initState(){
+    super.initState();
+
+    futureAmounts = getFutureAmounts(widget.holdingsViewModel.getWallets());
+    futureValues = getFutureValues(widget.holdingsViewModel.getWallets());
+  }
+
+  //widget.holdingsViewModel.getAmount(widget.holdingsViewModel.getWallets()[index])
+  List<Future<String>> getFutureAmounts(List<Wallet> wallets) {
+    
+    List<Future<String>> list = List<Future<String>>.empty(growable: true);
+    
+    for(Wallet wallet in wallets){
+      list.add(widget.holdingsViewModel.getAmountFuture(wallet));
+    }
+
+    return list;
+  }
+
+  //widget.holdingsViewModel.getValue(widget.holdingsViewModel.getWallets()[index], _currency)
+  List<Future<String>> getFutureValues(List<Wallet> wallets) {
+    
+    List<Future<String>> list = List<Future<String>>.empty(growable: true);
+    
+    for(Wallet wallet in wallets){
+      list.add(widget.holdingsViewModel.getValueFuture(wallet, _currency));
+    }
+
+    return list;
+  }
+
+
+  
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       body: Column(
         children: [
-          HeaderScreen(),
+          HeaderScreen(headerViewModel: widget.headerViewModel,),
           Placeholder(
             fallbackHeight: 200,
           ),
           Expanded(
             child: ListView.builder(
+              itemCount: widget.holdingsViewModel.getWallets().length,
               padding: EdgeInsets.all(10.0),
               shrinkWrap: false,
               itemBuilder: (BuildContext context, int index){
@@ -45,14 +92,60 @@ class _HoldingScreenState extends State<HoldingsScreen> {
         ),
     );
   }
-}
 
-Widget currencyItem(BuildContext context, int index){
+  Widget currencyItem(BuildContext context, int index){
   return Card(
     child: Row(
       children: <Widget>[
-        
+        const FlutterLogo(size: 10.0,),
+        Column(
+          children: [
+            Text(widget.holdingsViewModel.getWallets()[index].network),
+            // Text(widget.holdingsViewModel.getAmount(widget.holdingsViewModel.getWallets()[index]))
+            FutureBuilder<String?>(
+              future: futureAmounts[index],
+              builder: (context, snapshot){
+                if(snapshot.hasError){
+                  final error = snapshot.error;
+
+                  return Text('$error');
+                }
+                else if(snapshot.hasData){
+                  String data = snapshot.data!;
+
+                  return Text(data);
+                }
+                else{
+                  return const CircularProgressIndicator();
+                }
+              })
+          ],
+        ),
+        Row(
+          children: [
+            // Text(widget.holdingsViewModel.getValue(widget.holdingsViewModel.getWallets()[index], _currency))
+            FutureBuilder(
+              future: futureValues[index],
+              builder: (context, snapshot){
+                if(snapshot.hasError){
+                  final error = snapshot.error;
+
+                  return Text('$error');
+                }
+                else if(snapshot.hasData){
+                  String data = snapshot.data!;
+
+                  return Text(data);
+                }
+                else{
+                  return const CircularProgressIndicator();
+                }
+              })
+          ],
+        )
       ],
     ),
   );
 }
+}
+
